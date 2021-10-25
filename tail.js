c8f98e3250723328eb07abfdecc61a8f8436d1bd
Marvin Cayetano <cayetanomarvin@gmail.com>
2021-10-25 17:15:48 +0000

    tail.js

---
+++ tail.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+    return `Assertion ${JSON.stringify(actual) === JSON.stringify(expected) ? "Passed" : "Failed"}: ${actual} ${JSON.stringify(actual) === JSON.stringify(expected) ? "=" : "!" }== ${expected} 😇`;
+};
+
+const tail = function(arr) {
+    if(arr.length) {
+        return arr.splice(1);
+    }
+}
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+console.log(assertEqual(result.length, 2)); // ensure we get back two elements
+console.log(assertEqual(result[0], "Lighthouse")); // ensure first element is "Lighthouse"
+console.log(assertEqual(result[1], "Labs")); // ensure second element is "Labs"
+
+console.log(assertEqual(result, ["Lighthouse", "Labs"])); // => will always fail!
