d70259ceceefb36169eb929eaec868e4ed2ebfe5
Marvin Cayetano <cayetanomarvin@gmail.com>
2021-11-01 11:01:23 -0400

    🔥 Chore: lotide refactor:

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,6 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
-    return `Assertion ${actual === expected ? "Passed" : "Failed"}: ${actual} ${actual === expected ? "=" : "!" }== ${expected} 😇`;
-  };
+  return `Assertion ${actual === expected ? "Passed" : "Failed"}: ${actual} ${actual === expected ? "=" : "!" }== ${expected} 😇`;
+};
 
-  // TEST CODE
-  console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-  console.log(assertEqual(1, 1));
+module.exports = assertEqual;
--- head.js
+++ head.js
@@ -6,9 +6,4 @@ const head = function(arr) {
     return undefined;
 }
 
-const assertEqual = function(actual, expected) {
-    return `Assertion ${actual === expected ? "Passed" : "Failed"}: ${actual} ${actual === expected ? "=" : "!" }== ${expected} 😇`;
-  };
-
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+module.exports = head;
